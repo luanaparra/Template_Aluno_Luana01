f29cad7f58c22f1b08a07db532208128af115672
luanaparra <luana.silva@sou.inteli.edu.br>
2022-06-15 11:10:29 -0300

    update week 9

---
+++ 04_AUT_EST_EX_OBRIGATORIOS/semana 9/exercicio_1/script.js
@@ -0,0 +1,10 @@
+function mostrar(){
+    let n = document.getElementById("numero");
+    
+    if (n[0] % 2 === 0){
+        alert("O número na casa das centenas é: Par");
+    }
+    else{
+        alert("O número na casa das centenas é: Impar")
+    }
+}
